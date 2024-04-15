cbac5d38d923edf8887b41f027d8323742322b37
rakibtg <rakibtg@gmail.com>
2024-04-15 20:02:18 +0200

    Refactor Sidebar component to use Select component for filtering channels, channel groups, and campaigns

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,125 +1,71 @@
 "use client";
 
-import { Box } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
-import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import Select from "./Select";
 import TextLabel from "./TextLabel";
-
-const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-const checkedIcon = <CheckBoxIcon fontSize="small" />;
+import { Box } from "@mui/material";
+import { useData } from "@/providers/data.provider";
+import { useFilter } from "@/providers/filters.provider";
 
 export default function Sidebar() {
+  const { channels, campaigns, channelGroups } = useData();
+  const {
+    channelFilters,
+    campaignFilters,
+    channelGroupFilters,
+    setChannelsFilters,
+    setCampaignFilters,
+    setChannelGroupFilters,
+  } = useFilter();
+
   return (
-    <Box height="100%" display="flex" flexDirection="column">
+    <Box
+      height="100%"
+      display="flex"
+      flexDirection="column"
+      paddingBottom={{
+        xs: 2,
+        md: 0,
+      }}
+    >
       <Box
         flexGrow={1}
         marginBottom={1.5}
-        padding={0.5}
         display="flex"
         flexDirection={"column"}
-        gap={2.5}
+        gap={{
+          xs: 2,
+          md: 3,
+        }}
       >
         <TextLabel>Filter Report Data</TextLabel>
-        <Autocomplete
-          multiple
-          options={top100Films}
-          disableCloseOnSelect
-          getOptionLabel={(option) => option.title}
-          size="small"
-          renderOption={(props, option, { selected }) => (
-            <li {...props}>
-              <Checkbox
-                icon={icon}
-                checkedIcon={checkedIcon}
-                style={{ marginRight: 4 }}
-                checked={selected}
-              />
-              {option.title}
-            </li>
-          )}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Filter by Channels"
-              placeholder="Select one or more channels"
-            />
-          )}
-          style={{
-            backgroundColor: "#fcfcfc",
+
+        <Select
+          label="Channels"
+          options={channels}
+          value={channelFilters}
+          onChange={(items) => {
+            setChannelsFilters(items);
           }}
         />
 
-        <Autocomplete
-          multiple
-          options={top100Films}
-          disableCloseOnSelect
-          getOptionLabel={(option) => option.title}
-          size="small"
-          renderOption={(props, option, { selected }) => (
-            <li {...props}>
-              <Checkbox
-                icon={icon}
-                checkedIcon={checkedIcon}
-                style={{ marginRight: 4 }}
-                checked={selected}
-              />
-              {option.title}
-            </li>
-          )}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Filter by Channel Groups"
-              placeholder="Select one or more channel groups"
-            />
-          )}
-          style={{
-            backgroundColor: "white",
+        <Select
+          label="Channel Groups"
+          options={channelGroups}
+          value={channelGroupFilters}
+          onChange={(items) => {
+            setChannelGroupFilters(items);
           }}
         />
 
-        <Autocomplete
-          multiple
-          options={top100Films}
-          disableCloseOnSelect
-          getOptionLabel={(option) => option.title}
-          size="small"
-          renderOption={(props, option, { selected }) => (
-            <li {...props}>
-              <Checkbox
-                icon={icon}
-                checkedIcon={checkedIcon}
-                style={{ marginRight: 4 }}
-                checked={selected}
-              />
-              {option.title}
-            </li>
-          )}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Filter by Campaigns"
-              placeholder="Select one or more campaigns"
-            />
-          )}
-          style={{
-            backgroundColor: "white",
+        <Select
+          label="Campaigns"
+          options={campaigns}
+          value={campaignFilters}
+          onChange={(items) => {
+            setCampaignFilters(items);
           }}
         />
       </Box>
     </Box>
   );
 }
-
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-];
