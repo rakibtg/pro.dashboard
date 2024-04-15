9eb24c93ab9b1303fd1ef62174891aaef0c1b780
rakibtg <rakibtg@gmail.com>
2024-04-15 20:01:28 +0200

    Update Navbar component with Share button and copyUrl functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { Box, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Snackbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import colors from "../utils/colors.util";
+import { ShareOutlined } from "@mui/icons-material";
+import { copyUrl } from "@/utils/copyUrl";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <Box
       paddingY={2}
@@ -12,19 +24,40 @@ export default function Navbar() {
       marginBottom={2}
     >
       <Container maxWidth="lg">
-        <Typography
-          fontWeight="bold"
-          letterSpacing={2}
-          fontSize={16}
-          textTransform="uppercase"
-          sx={{ cursor: "pointer", textDecoration: "none" }}
-          component={"a"}
-          href="/"
-          color={colors.brand}
-        >
-          Pro.ai
-        </Typography>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography
+            fontWeight="bold"
+            letterSpacing={2}
+            fontSize={16}
+            textTransform="uppercase"
+            sx={{ cursor: "pointer", textDecoration: "none" }}
+            component={"a"}
+            href="/"
+            color={colors.brand}
+          >
+            Pro.ai
+          </Typography>
+          <Tooltip title="Copy link to this dashboard">
+            <Button
+              variant="outlined"
+              startIcon={<ShareOutlined />}
+              onClick={() => {
+                copyUrl();
+                setOpen(true);
+              }}
+            >
+              Share
+            </Button>
+          </Tooltip>
+        </Box>
       </Container>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        onClose={() => setOpen(false)}
+        message="Copied link for this dashboard"
+      />
     </Box>
   );
 }
